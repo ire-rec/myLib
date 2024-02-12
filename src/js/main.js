55aee8f11e8a6196213e20152c62f255d3699b57
ire-rec <i.recavarren@gmail.com>
2024-02-12 14:08:35 +0100

    add global var to js

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,5 @@
 "use strict";
+// GLOBAL VAR
+const inputTitle = document.querySelector(".inputTitle_js");
+const inputSearch = document.querySelector(".inputSearch_js");
+const ulBooks = document.querySelector(".ul_books_js");
