28b6e680ec962b134635f416e2b6ba0972cb20ba
ire-rec <i.recavarren@gmail.com>
2024-02-14 13:22:08 +0100

    add getData function

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,18 @@
 const inputTitle = document.querySelector(".inputTitle_js");
 const inputSearch = document.querySelector(".inputSearch_js");
 const ulBooks = document.querySelector(".ul_books_js");
+
+// DATOS GLOBALES
+let data = [];
+
+//FUNCTIONS
+const getData = () => {
+  let search = inputTitle.value;
+  fetch(`https://openlibrary.org/search.json?q=${search}`)
+    .then((response) => response.json())
+    .then((json) => {
+      data = json.docs;
+      console.log(data);
+      renderBooks();
+    });
+};
