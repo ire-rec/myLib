6f3915d29a61c56e1fe76a49359acc70a47442cd
ire-rec <i.recavarren@gmail.com>
2024-02-14 13:26:00 +0100

    add renderBooks and handleClick functions

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,29 @@ const ulBooks = document.querySelector(".ul_books_js");
 let data = [];
 
 //FUNCTIONS
+
+const renderBooks = () => {
+  let html = "";
+
+  for (const item of data) {
+    if (item.cover_edition_key === undefined) {
+      html += `<div class="div hide"><li><img src="https://covers.openlibrary.org/b/olid/${item.cover_edition_key}.jpg" alt="book cover" class="cover_book"/></li>`;
+    } else {
+      html += `<div class="div"><li><img src="https://covers.openlibrary.org/b/olid/${item.cover_edition_key}.jpg" alt="book cover" class="cover_book"/></li>`;
+    }
+    html += `<li class="title">${item.title}</li>`;
+    html += `<li class="author">${item.author_name}</li></div>`;
+  }
+  ulBooks.innerHTML = html;
+};
+
+const handleClick = (ev) => {
+  console.log("funciona");
+  getData();
+};
+
+inputSearch.addEventListener("click", handleClick);
+
 const getData = () => {
   let search = inputTitle.value;
   fetch(`https://openlibrary.org/search.json?q=${search}`)
